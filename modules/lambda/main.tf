935df641f272b380592ecdd758d645c14b167bc1
narendiransankar <narendiransankar1699@gmail.com>
2025-07-15 22:40:10 +0530

    Create main.tf

---
+++ modules/lambda/main.tf
@@ -0,0 +1,60 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = var.lambda_source
+  output_path = "${path.module}/${var.function_name}.zip"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.function_name}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "ec2_policy" {
+  name = "${var.function_name}-ec2-policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = var.action == "start" ? ["ec2:StartInstances"] : ["ec2:StopInstances"]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+
+resource "aws_lambda_function" "this" {
+  function_name    = var.function_name
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
+  handler          = var.handler
+  runtime          = var.runtime
+  role             = aws_iam_role.lambda_role.arn
+
+  environment {
+    variables = {
+      INSTANCE_ID = var.ec2_instance_id
+    }
+  }
+}
