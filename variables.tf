77f866de61781ad6a188922748b532d343fbd6be
narendiransankar <narendiransankar1699@gmail.com>
2025-07-15 22:36:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,13 @@
 variable "region" {}
-variable "instance_type" { default = "t3.micro" }
 variable "ami_id" {}
-variable "instance_count" { default = 1 }
-variable "start_cron" {
-  description = "CloudWatch CRON for EC2 start (8AM IST: cron(30 2 * * ? *))"
+variable "instance_type" { 
+  default = "t3.micro" 
 }
-variable "stop_cron" {
-  description = "CloudWatch CRON for EC2 stop (5PM IST: cron(30 11 * * ? *))"
+variable "instance_count" { 
+  default = 1 
 }
+variable "lambda_runtime" { 
+  default = "python3.12" 
+}
+variable "start_cron" {}
+variable "stop_cron" {}
