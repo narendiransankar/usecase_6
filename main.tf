056463816b7905750dad2255ec24c039332040e8
narendiransankar <narendiransankar1699@gmail.com>
2025-07-15 22:35:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,50 +3,42 @@ provider "aws" {
 }
 
 module "ec2" {
-  source        = "./modules/mod-ec2"
-  instance_type = var.instance_type
-  ami_id        = var.ami_id
+  source         = "./modules/ec2"
+  instance_type  = var.instance_type
+  ami_id         = var.ami_id
   instance_count = var.instance_count
 }
 
-module "iam" {
-  source = "./modules/mod-iam"
-}
-
 module "lambda_start" {
-  source          = "./modules/mod-lambda"
-  function_name   = "start-ec2"
-  handler         = "ec2_scheduler.lambda_handler"
-  runtime         = "python3.12"
-  lambda_role_arn = module.iam.lambda_role_arn
-  ec2_instance_ids = module.ec2.instance_ids
-  action_type      = "start"
+  source             = "./modules/lambda"
+  function_name      = "start-ec2"
+  handler            = "start_ec2.lambda_handler"
+  runtime            = var.lambda_runtime
+  lambda_source      = "${path.module}/modules/lambda/start_ec2.py"
+  ec2_instance_id    = module.ec2.instance_id # Single instance, update for list if needed
+  action             = "start"
 }
 
 module "lambda_stop" {
-  source          = "./modules/mod-lambda"
-  function_name   = "stop-ec2"
-  handler         = "ec2_scheduler.lambda_handler"
-  runtime         = "python3.12"
-  lambda_role_arn = module.iam.lambda_role_arn
-  ec2_instance_ids = module.ec2.instance_ids
-  action_type      = "stop"
+  source             = "./modules/lambda"
+  function_name      = "stop-ec2"
+  handler            = "stop_ec2.lambda_handler"
+  runtime            = var.lambda_runtime
+  lambda_source      = "${path.module}/modules/lambda/stop_ec2.py"
+  ec2_instance_id    = module.ec2.instance_id # Single instance, update for list if needed
+  action             = "stop"
 }
 
 module "scheduler_start" {
-  source              = "./modules/mod-scheduler"
-  rule_name           = "start-ec2-rule"
-  schedule_expression = var.start_cron
-  lambda_function_arn = module.lambda_start.lambda_function_arn
-  action_type         = "start"
-  ec2_instance_ids    = module.ec2.instance_ids
+  source               = "./modules/scheduler"
+  rule_name            = "start-ec2-schedule"
+  schedule_expression  = var.start_cron
+  lambda_function_arn  = module.lambda_start.lambda_function_arn
 }
 
 module "scheduler_stop" {
-  source              = "./modules/mod-scheduler"
-  rule_name           = "stop-ec2-rule"
-  schedule_expression = var.stop_cron
-  lambda_function_arn = module.lambda_stop.lambda_function_arn
-  action_type         = "stop"
-  ec2_instance_ids    = module.ec2.instance_ids
+  source               = "./modules/scheduler"
+  rule_name            = "stop-ec2-schedule"
+  schedule_expression  = var.stop_cron
+  lambda_function_arn  = module.lambda_stop.lambda_function_arn
 }
