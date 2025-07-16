56c2c7cf1a8bbe26e34b748adc7791fee806bea8
narendiransankar <narendiransankar1699@gmail.com>
2025-07-16 23:11:37 +0530

    Update basic_test.go

--- test/basic_test.go
+++ test/basic_test.go
@@ -1,28 +1,20 @@
-package test
-
 import (
-  "testing"
-  "github.com/gruntwork-io/terratest/modules/terraform"
-  "github.com/stretchr/testify/assert"
+    "testing"
+    "github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/stretchr/testify/assert"
 )
 
 func TestTerraformBasic(t *testing.T) {
-  t.Parallel()
+    terraformOptions := &terraform.Options{
+        TerraformDir: "../", // or "." depending on your repo structure
+    }
 
-  terraformOptions := &terraform.Options{
-    TerraformDir: "../", // Pointing to the root module
-    Vars: map[string]interface{}{
-      // Example: "bucket_name": "test-bucket-name"
-    },
-  }
+    defer terraform.Destroy(t, terraformOptions)
 
-  // At the end, destroy infra
-  defer terraform.Destroy(t, terraformOptions)
-
-  // Init and Apply
-  terraform.InitAndApply(t, terraformOptions)
-
-  // Example: check output variable (if you have one)
+    terraform.InitAndApply(t, terraformOptions)
+    // Example: check output variable (if you have one)
   // output := terraform.Output(t, terraformOptions, "bucket_id")
   // assert.NotEmpty(t, output)
+    // Dummy assertion, always passes
+    assert.Equal(t, 1, 1)
 }
